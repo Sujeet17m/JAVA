fdb50e05de5685675b3f6e94bf9b1311fcfbd755
Sujeet17m <sujeet17das@gmail.com>
2025-07-19 09:42:05 +0530

    Scanner input

--- Main.java
+++ Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        try (Scanner input = new Scanner(System.in)){
+            System.out.println(input.nextInt());
+        }
+        
     }
 }
+
