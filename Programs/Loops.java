9d040b5ebd0f689324d26a4791711698f538e45a
Sujeet17m <sujeet17das@gmail.com>
2025-07-21 07:22:54 +0530

    While Loop

--- Programs/Loops.java
+++ Programs/Loops.java
@@ -11,11 +11,19 @@ public class Loops {
 
         //print number 1 to n
         try(Scanner in = new Scanner(System.in)) {
-        System.out.print("Please enter a number: ");
+        //System.out.print("Please enter a number: ");
         int n = in.nextInt();
         for(int i =1; i <=n; i++){
-            System.out.println("Number:" + i);
+           // System.out.println("Number:" + i);
         }
     }
-}
+        //while loop
+        //Syntax: while(condition){statement}
+        int i =1;
+        while(i <= 17){
+            System.out.println("Number: " + i);
+            i++;
+        }
+    }
+
 }
