65f622ebe7f9389004de5212b784aad3362c430b
Sujeet17m <sujeet17das@gmail.com>
2025-07-21 07:43:23 +0530

    Loops completed

--- Programs/Loops.java
+++ Programs/Loops.java
@@ -16,14 +16,40 @@ public class Loops {
         for(int i =1; i <=n; i++){
            // System.out.println("Number:" + i);
         }
-    }
-        //while loop
-        //Syntax: while(condition){statement}
-        int i =1;
-        while(i <= 17){
-            System.out.println("Number: " + i);
-            i++;
+
+        System.out.print("Please enter a number (while loop): ");
+        int num = in.nextInt();
+        int j = 1;
+        while (j <= num) {
+            System.out.println("Number (while loop): " + j);
+            j++;
+        }
+        in.close();
         }
     }
+        // while loop
+        // Syntax: while(condition){statement}
+        // int i =1;
+        
+        // while(i <= 17){
+        //     //System.out.println("Number: " + i);
+        //     i++;
+        // }
 
-}
+        // try(Scanner sc = new Scanner(System.in)){
+        //     System.out.print("Please enter a number: ");
+        //     int num = sc.nextInt();
+        //     int j = 1;
+        //     while(j <= num){
+        //         System.out.println("Number: " + j);
+        //         j++;
+        //     }
+        // }
+        // Print numbers from 1 to num using while loop
+        // do-while loop
+        // Syntax: do{statement}while(condition);
+
+        
+    }
+
+
