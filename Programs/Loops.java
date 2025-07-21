c8430c69685eda49d54b1e05f0f40737f3e6ed8c
Sujeet17m <sujeet17das@gmail.com>
2025-07-21 07:18:55 +0530

    for loop

---
+++ Programs/Loops.java
@@ -0,0 +1,21 @@
+package Programs;
+
+import java.util.Scanner;
+
+//loops
+public class Loops {
+    public static void main(String[] args) {
+        for (int num = 1; num <= 5; num++) {
+           // System.out.println("Number: " + num);
+        }
+
+        //print number 1 to n
+        try(Scanner in = new Scanner(System.in)) {
+        System.out.print("Please enter a number: ");
+        int n = in.nextInt();
+        for(int i =1; i <=n; i++){
+            System.out.println("Number:" + i);
+        }
+    }
+}
+}
