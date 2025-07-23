00bdea4c6d5a1facead4d234263bd904bab90a97
Sujeet17m <sujeet17das@gmail.com>
2025-07-23 06:11:55 +0530

    new style to write switch case

--- SwitchExample.java
+++ SwitchExample.java
@@ -6,16 +6,24 @@ public class SwitchExample {
         int input = sc.nextInt();
         
 // example  old switch statement
+        // switch (input){
+        // case 1:
+        //     System.out.println("You entered one.");
+        //     break;
+        // case 2:
+        //     System.out.println("you entered 2.");
+        //     break;
+        // default:
+        //     System.out.println("You entered something else.");
+        //     break;
+        // }
+
+        // example new switch statement
+        // Note: This is a feature introduced in Java 12, so ensure your environment supports
         switch (input){
-        case 1:
-            System.out.println("You entered one.");
-            break;
-        case 2:
-            System.out.println("you entered 2.");
-            break;
-        default:
-            System.out.println("You entered something else.");
-            break;
+            case 1 -> System.out.println("You entered one.");
+            case 2 -> System.out.println("You entered two.");
+            default -> System.out.println("You entered something else.");
         }
     }
     
