9dbd0207408f4cda97ca9df36faa3edb3d0a9126
Sujeet17m <sujeet17das@gmail.com>
2025-07-23 06:08:59 +0530

    old switch case style

---
+++ SwitchExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SwitchExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        
+// example  old switch statement
+        switch (input){
+        case 1:
+            System.out.println("You entered one.");
+            break;
+        case 2:
+            System.out.println("you entered 2.");
+            break;
+        default:
+            System.out.println("You entered something else.");
+            break;
+        }
+        
+    }
+    }
+    
+
+    
+
+	
