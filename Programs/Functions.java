11033c122467d84bf74c5f2fc7c299bc7d83e5e5
Sujeet17m <sujeet17das@gmail.com>
2025-07-28 01:38:27 +0530

    void and int

--- Programs/Functions.java
+++ Programs/Functions.java
@@ -3,20 +3,51 @@ package Programs;
 import java.util.Scanner;
 
 public class Functions {
-    public static void main(String[] args) {
-        try(Scanner in = new Scanner(System.in)) {
-            System.out.print("Enter first number: ");
-            int num1 = in.nextInt();
-            System.out.print("Enter second number: ");
-            int num2 = in.nextInt();
+    // public static void main(String[] args) {
+    //     try(Scanner in = new Scanner(System.in)) {
+    //         System.out.print("Enter first number: ");
+    //         int num1 = in.nextInt();
+    //         System.out.print("Enter second number: ");
+    //         int num2 = in.nextInt();
             
-            int result = add(num1, num2);
-            System.out.println("Sum: " + result);
+    //         int result = add(num1, num2);
+    //         System.out.println("Sum: " + result);
+    //     }
+    // }
+
+    // // Method to add two integers
+    // public static int add(int a, int b) {
+    //     return a + b;
+    // }
+    public static void main(String[] args) {
+        
+        int ans = sum2();
+        System.out.println(ans);
+        
+    }
+    //return the value of sum
+    static int sum2(){
+        try(Scanner input = new Scanner(System.in)){
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            float sum = num1 + num2;
+            System.out.println("sum:" + sum);
+            //typecasting
+            int num = (int)(65.56);
+            System.out.println(num);
+            return (int)sum; // returning the sum as an integer
         }
     }
-
-    // Method to add two integers
-    public static int add(int a, int b) {
-        return a + b;
+    // Example of a method that could be added
+    static void sum(){
+        try(Scanner input = new Scanner(System.in)){
+            int num1 = input.nextInt();
+            int num2 = input.nextInt();
+            float sum = num1 + num2;
+            System.out.println("sum:" + sum);
+            //typecasting
+            int num = (int)(65.56);
+            System.out.println(num);
+        }
     }
 }
