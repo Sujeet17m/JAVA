bf00ba533655216cc08f86dffabdbc2d154cbeb0
Sujeet17m <sujeet17das@gmail.com>
2025-07-27 23:17:51 +0530

    function = method

--- Programs/Functions.java
+++ Programs/Functions.java
@@ -1,5 +1,22 @@
 package Programs;
 
+import java.util.Scanner;
+
 public class Functions {
-    
+    public static void main(String[] args) {
+        try(Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter first number: ");
+            int num1 = in.nextInt();
+            System.out.print("Enter second number: ");
+            int num2 = in.nextInt();
+            
+            int result = add(num1, num2);
+            System.out.println("Sum: " + result);
+        }
+    }
+
+    // Method to add two integers
+    public static int add(int a, int b) {
+        return a + b;
+    }
 }
