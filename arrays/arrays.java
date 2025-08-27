383bd556068dec3d937c01787a5d94114866bcd1
Sujeet17m <sujeet17das@gmail.com>
2025-08-27 21:33:46 +0530

    without array

---
+++ arrays/arrays.java
@@ -0,0 +1,13 @@
+public class arrays {
+    public static void main(String[] args) {
+        // store 5 roll number
+        int rno1 = 23;
+        int rno2 = 22;
+        int rno3 = 21;
+        // for how much u'll write so here array comes to play
+        
+        
+         
+
+    }
+}
