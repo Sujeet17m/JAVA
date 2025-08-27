328176fea7dfda69448a62fbae28204746e0b432
Sujeet17m <sujeet17das@gmail.com>
2025-08-27 21:37:11 +0530

    with array

--- arrays/arrays.java
+++ arrays/arrays.java
@@ -5,6 +5,9 @@ public class arrays {
         int rno2 = 22;
         int rno3 = 21;
         // for how much u'll write so here array comes to play
+        int[] rnos = new int[5];
+        // or directly
+        int[] rnos2 = {23,22,21,56,78};
         
         
          
