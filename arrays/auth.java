532afbd0e253fd929fb650a89c0bd056ce4dba16
Sujeet17m <sujeet17das@gmail.com>
2025-09-02 21:17:10 +0530

    database array

--- arrays/auth.java
+++ arrays/auth.java
@@ -1,23 +1,44 @@
 import java.util.Scanner;
 
 public class auth {
+    public String username;
+    public String password;
+
+    public auth(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
     public static void main(String[] args) {
-        String username = "admin";
-        String password = "secret";
-
+        auth database = new auth("Sujeet", "Secret");
         try(Scanner scanner = new Scanner(System.in)){
-            System.out.print("Enter username: ");
+            System.out.println("Enter username: ");
             String inputUsername = scanner.nextLine();
-            System.out.print("Enter password: ");
+            System.out.println("Enter password: ");
             String inputPassword = scanner.nextLine();
-
-            if (username.equals(inputUsername) && password.equals(inputPassword)) {
+            if (database.username.equals(inputUsername) && database.password.equals(inputPassword)) {
                 System.out.println("Login successful!");
             } else {
                 System.out.println("Invalid credentials.");
             }
         }
         
+        // System.out.println(database.username);
+        // System.out.println(database.password);
+        // String username = "admin";
+        // String password = "secret";
+
+        // try(Scanner scanner = new Scanner(System.in)){
+        //     System.out.print("Enter username: ");
+        //     String inputUsername = scanner.nextLine();
+        //     System.out.print("Enter password: ");
+        //     String inputPassword = scanner.nextLine();
+
+        //     if (username.equals(inputUsername) && password.equals(inputPassword)) {
+        //         System.out.println("Login successful!");
+        //     } else {
+        //         System.out.println("Invalid credentials.");
+        //     }
+        // }
     }
-    
 }
