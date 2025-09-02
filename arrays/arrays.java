4d43cad343c7d4ffaf48096bb48385b7841cdf7b
Sujeet17m <sujeet17das@gmail.com>
2025-09-02 20:41:20 +0530

    arrays

--- arrays/arrays.java
+++ arrays/arrays.java
@@ -8,9 +8,9 @@ public class arrays {
         int[] rnos = new int[5];
         // or directly
         int[] rnos2 = {23,22,21,56,78};
-        
-        
-         
+
+        int[] ros; // declaration of array. ros is getting defined in the stack
+        ros = new int[5]; // initialization: actually here object is getting created in the heap
 
     }
 }
