3763532cd07f4713b9d3c7e7b946bec3fe8e4ec7
Sujeet17m <sujeet17das@gmail.com>
2025-09-05 18:58:54 +0530

    scanner

--- arrays/Multidimensional.java
+++ arrays/Multidimensional.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 public class Multidimensional {
     public static void main(String[] args) {
-        int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        array[1][2] = 10;
-        System.out.println("Updated array:");
-        for (int[] row : array) {
-            for (int num : row) {
-                System.out.print(num + " ");
+        // int[][] array = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        // array[1][2] = 10;
+        // System.out.println("Updated array:");
+        // for (int[] row : array) {
+        //     for (int num : row) {
+        //         System.out.print(num + " ");
+        //     }
+        //     System.out.println();
+
+        try(Scanner in = new Scanner(System.in)) {
+            
+        int[][] arr = new int[3][3];
+        System.out.println(arr.length); // output: 3
+
+        //input
+        for(int row = 0; row < arr.length; row++){
+            // for each col in every row
+            for(int col =0; col< arr[row].length; col ++){
+                System.out.print("Enter number for row " + row + " and col " + col + ": ");
+                arr[row][col] = in.nextInt();
+
             }
-            System.out.println();
+        }
+        }
     
     }
     
-}
 }
