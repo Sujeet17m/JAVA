6c55080b8bdcc07eb846d23d16bc97a16044a398
Sujeet17m <sujeet17das@gmail.com>
2025-09-05 19:32:27 +0530

    ArrayList all functions

---
+++ arrays/ArrayListExample.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        //Syntax
+        ArrayList<Integer> list = new ArrayList<>(5);
+        //Adding elements
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        //Displaying all elements
+        for(int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        //Updating an element
+        list.set(2, 60);
+        System.out.println("After updating: " + list.get(2));
+        //Removing an element
+        list.remove(3);
+        System.out.println("After removing: " + list);
+        //Checking if the list contains a specific element
+        System.out.println("Contains 40: " + list.contains(40));
+        //Checking the size of the list
+        System.out.println("Size of the list: " + list.size());
+        //Clearing the list
+        list.clear();
+        System.out.println("After clearing: " + list);
+
+    }
+    
+}
--- arrays/colnofixed.java
+++ arrays/colnofixed.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class colnofixed {
     public static void main(String[] args) {
         int[][] arr = {{1,2},{3,4,5,6},{7,8,9}};
@@ -5,6 +7,7 @@ public class colnofixed {
         for(int row = 0; row < arr.length; row++){
             for (int col = 0 ; col < arr[row].length; col++){
                 System.out.print(arr[row][col] + " ");
+                System.out.println(Arrays.toString(arr));
             }
         
             System.out.println();
