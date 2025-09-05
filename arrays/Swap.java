e8a6757b1e93c9c37e1f4deaba3e95be0b0f66b5
Sujeet17m <sujeet17das@gmail.com>
2025-09-05 19:56:44 +0530

    Swapping values in array

---
+++ arrays/Swap.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+        swap(arr, 0, 3);
+
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
+    
+}
